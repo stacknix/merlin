add6af62c3f1bcc18ca924a06ff24ffcd17cbb4a
Ashish Sahu <ashish.sahu7@t-mobile.com>
2021-12-16 03:36:30 +0530

    snapshot

--- merlin/src/main/java/io/stacknix/merlin/db/MerlinQuery.java
+++ merlin/src/main/java/io/stacknix/merlin/db/MerlinQuery.java
@@ -3,6 +3,7 @@ package io.stacknix.merlin.db;
 
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import io.stacknix.merlin.db.annotations.Order;
 import io.stacknix.merlin.db.annotations.SortKey;
@@ -31,6 +32,22 @@ public class MerlinQuery<T extends MerlinObject> extends Filter {
 
     public MerlinResult<T> find() {
         return Merlin.getInstance().db().search(this);
+    }
+
+    public void observe(@NotNull ResultChangeListener<T> resultChangeListener) {
+        MerlinResult<T> result = find();
+        resultChangeListener.onChange(result);
+        result.listen(resultChangeListener);
+    }
+
+    public void observeObject(@NotNull ObjectChangeListener<T> objectChangeListener) {
+        MerlinResult<T> result = find();
+        if (result.isEmpty()) {
+            objectChangeListener.onChange(null);
+        } else {
+            objectChangeListener.onChange(result.get(0));
+        }
+        result.listenObject(objectChangeListener);
     }
 
     public MerlinQuery<T> limit(int count) {
