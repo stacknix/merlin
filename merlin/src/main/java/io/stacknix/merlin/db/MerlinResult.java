f0264b0720aba8fe1bdabe0fed00c8f7b9fe15ef
Ashish Sahu <ashish.sahu7@t-mobile.com>
2021-12-16 02:01:32 +0530

    snapshot

--- merlin/src/main/java/io/stacknix/merlin/db/MerlinResult.java
+++ merlin/src/main/java/io/stacknix/merlin/db/MerlinResult.java
@@ -22,66 +22,32 @@ public class MerlinResult<T extends MerlinObject> extends ArrayList<T> {
 
     public void listen(ResultChangeListener<T> listener) {
         this.resultChangeListener = listener;
-        dispatchResult(query.find(), true);
         Merlin.getInstance().listen((tClass, operation) -> {
             if (tClass == query.getObjectClass()) {
-                dispatchResult(query.find(), true);
+                dispatchResult(query.find());
             }
         });
     }
 
     public void listenObject(ObjectChangeListener<T> listener) {
         this.objectChangeListener = listener;
-        dispatchResult(query.find(), true);
         Merlin.getInstance().listen((tClass, operation) -> {
             if (tClass == query.getObjectClass()) {
-                dispatchResult(query.find(), true);
+                dispatchResult(query.find());
             }
         });
     }
 
-    public void observe(ResultChangeListener<T> listener) {
-        this.resultChangeListener = listener;
-        dispatchResult(query.find(), false);
-        Merlin.getInstance().listen((tClass, operation) -> {
-            if (tClass == query.getObjectClass()) {
-                dispatchResult(query.find(), false);
-            }
-        });
-    }
-
-    public void observeObject(ObjectChangeListener<T> listener) {
-        this.objectChangeListener = listener;
-        dispatchResult(query.find(), false);
-        Merlin.getInstance().listen((tClass, operation) -> {
-            if (tClass == query.getObjectClass()) {
-                dispatchResult(query.find(), false);
-            }
-        });
-    }
-
-    private void dispatchResult(MerlinResult<T> result, boolean mainThread) {
+    private void dispatchResult(MerlinResult<T> result) {
         if (resultChangeListener != null) {
             Logging.i(TAG, "Dispatching Results");
-            if (!mainThread) {
-                resultChangeListener.onChange(result);
-            } else {
-                new Handler(Looper.getMainLooper()).post(() -> resultChangeListener.onChange(result));
-            }
+            resultChangeListener.onChange(result);
         } else if (objectChangeListener != null) {
             Logging.i(TAG, "Dispatching Object");
-            if (!mainThread) {
-                if (result.isEmpty()) {
-                    objectChangeListener.onChange(null);
-                } else {
-                    objectChangeListener.onChange(result.get(0));
-                }
+            if (result.isEmpty()) {
+                objectChangeListener.onChange(null);
             } else {
-                if (result.isEmpty()) {
-                    new Handler(Looper.getMainLooper()).post(() -> objectChangeListener.onChange(null));
-                } else {
-                    new Handler(Looper.getMainLooper()).post(() -> objectChangeListener.onChange(result.get(0)));
-                }
+                objectChangeListener.onChange(result.get(0));
             }
         }
     }
