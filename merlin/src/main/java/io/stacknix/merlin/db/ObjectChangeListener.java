d33e05bab3318f7a2ca09f531e57f2bad22d77e7
Ashish Sahu <ashish.sahu7@t-mobile.com>
2021-12-15 10:21:12 +0530

    snapshot

--- merlin/src/main/java/io/stacknix/merlin/db/ObjectChangeListener.java
+++ merlin/src/main/java/io/stacknix/merlin/db/ObjectChangeListener.java
@@ -1,5 +1,7 @@
 package io.stacknix.merlin.db;
 
+import org.jetbrains.annotations.Nullable;
+
 public interface ObjectChangeListener <T extends MerlinObject> {
-    void onChange(T data);
+    void onChange(@Nullable T data);
 }
