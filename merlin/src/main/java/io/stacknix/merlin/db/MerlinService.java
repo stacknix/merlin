382d3b02d57b1429176880b5cfbe570e126e7826
Ashish Sahu <ashish.sahu7@t-mobile.com>
2021-12-18 09:49:03 +0530

    exception handler added

--- merlin/src/main/java/io/stacknix/merlin/db/MerlinService.java
+++ merlin/src/main/java/io/stacknix/merlin/db/MerlinService.java
@@ -11,7 +11,7 @@ import io.stacknix.merlin.db.commons.RecordNotFound;
 import io.stacknix.merlin.db.commons.ResultCompare;
 
 @SuppressWarnings({"UnusedReturnValue", "unused"})
-public abstract class MerlinService<T extends MerlinObject, I>{
+public abstract class MerlinService<T extends MerlinObject, I> {
 
     private final Class<T> tClass;
 
@@ -29,74 +29,89 @@ public abstract class MerlinService<T extends MerlinObject, I>{
 
     public abstract List<T> onSearch(Context context, MerlinQuery<T> query) throws Exception;
 
+    public abstract void onException(Exception exception);
+
     public Class<T> getObjectClass() {
         return tClass;
     }
 
-    public void read(Context context, I key) throws Exception {
-        T remoteItem = onRead(context, key);
-        DBAdapter<?> db = Merlin.getInstance().db();
-        T localItem = db.read(tClass, remoteItem.getPrimaryValue());
-        if (localItem != null) {
-            db.write(tClass, Collections.singletonList(remoteItem));
-        } else {
-            db.create(tClass, Collections.singletonList(remoteItem));
+    public void read(Context context, I key) {
+        try {
+            T remoteItem = onRead(context, key);
+            DBAdapter<?> db = Merlin.getInstance().db();
+            T localItem = db.read(tClass, remoteItem.getPrimaryValue());
+            if (localItem != null) {
+                db.write(tClass, Collections.singletonList(remoteItem));
+            } else {
+                db.create(tClass, Collections.singletonList(remoteItem));
+            }
+        } catch (Exception e) {
+            onException(e);
         }
     }
 
-    public void search(Context context) throws Exception {
+    public void search(Context context) {
         search(context, new MerlinQuery<>(getObjectClass()));
     }
 
-    public void search(Context context, MerlinQuery<T> query) throws Exception {
-        DBAdapter<?> db = Merlin.getInstance().db();
-        List<T> localList = db.search(query);
-        List<T> remoteList = onSearch(context, query);
-        ResultCompare<T> result = new ResultCompare<>(localList, remoteList);
-        db.delete(tClass, result.getDeleteData());
-        db.write(tClass, result.getWriteData());
-        db.create(tClass, result.getCreateData());
+    public void search(Context context, MerlinQuery<T> query) {
+        try {
+            DBAdapter<?> db = Merlin.getInstance().db();
+            List<T> localList = db.search(query);
+            List<T> remoteList = onSearch(context, query);
+            ResultCompare<T> result = new ResultCompare<>(localList, remoteList);
+            db.delete(tClass, result.getDeleteData());
+            db.write(tClass, result.getWriteData());
+            db.create(tClass, result.getCreateData());
+        } catch (Exception e) {
+            onException(e);
+        }
     }
 
     /**
      * this method sync existing record CREATE | WRITE | UNLINK
+     *
      * @param context
      * @throws Exception
      */
-    public synchronized void synchronize(Context context) throws Exception {
-        final String TAG = "MerlinService";
-        for (T item : Merlin.where(tClass).find()) {
-            switch (item.getFlag()) {
-                case Flag.NEED_CREATE:
-                    T newItem = onCreate(context, item);
-                    newItem.setFlag(Flag.IDLE);
-                    Logging.i(TAG, "Updating local flag to idle.[01]");
-                    newItem.save();
-                    break;
-                case Flag.NEED_WRITE:
-                    try {
-                        onWrite(context, item);
-                        item.setFlag(Flag.IDLE);
-                        Logging.i(TAG, "Updating local flag to idle.[11]");
-                        item.save();
-                    } catch (RecordNotFound e) {
-                        Logging.w(TAG, e.message);
-                        Logging.i(TAG, "Deleting from local.[11]");
-                        item.delete();
-                    }
-                    break;
-                case Flag.NEED_UNLINK:
-                    try {
-                        onUnlink(context, item);
-                        Logging.i(TAG, "Deleting from local.[21]");
-                        item.delete();
-                    } catch (RecordNotFound e) {
-                        Logging.w(TAG, e.message);
-                        Logging.i(TAG, "Deleting from local.[22]");
-                        item.delete();
-                    }
-                    break;
+    public synchronized void synchronize(Context context) {
+        try {
+            final String TAG = "MerlinService";
+            for (T item : Merlin.where(tClass).find()) {
+                switch (item.getFlag()) {
+                    case Flag.NEED_CREATE:
+                        T newItem = onCreate(context, item);
+                        newItem.setFlag(Flag.IDLE);
+                        Logging.i(TAG, "Updating local flag to idle.[01]");
+                        newItem.save();
+                        break;
+                    case Flag.NEED_WRITE:
+                        try {
+                            onWrite(context, item);
+                            item.setFlag(Flag.IDLE);
+                            Logging.i(TAG, "Updating local flag to idle.[11]");
+                            item.save();
+                        } catch (RecordNotFound e) {
+                            Logging.w(TAG, e.message);
+                            Logging.i(TAG, "Deleting from local.[11]");
+                            item.delete();
+                        }
+                        break;
+                    case Flag.NEED_UNLINK:
+                        try {
+                            onUnlink(context, item);
+                            Logging.i(TAG, "Deleting from local.[21]");
+                            item.delete();
+                        } catch (RecordNotFound e) {
+                            Logging.w(TAG, e.message);
+                            Logging.i(TAG, "Deleting from local.[22]");
+                            item.delete();
+                        }
+                        break;
+                }
             }
+        } catch (Exception e) {
+            onException(e);
         }
     }
 
